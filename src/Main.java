211b3ff0fae6d1334337e3358036f9a2f04ba5ba
Volkov Vadim <vad.volkov91@gmail.com>
2024-03-14 19:00:18 +0700

    upd 14.03

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,20 @@ public class Main {
         GameProgress game1 = new GameProgress(10, 20, 30, 40.5);
         GameProgress game2 = new GameProgress(100, 200, 300, 400.5);
         GameProgress game3 = new GameProgress(1000, 2000, 3000, 4000.5);
-        saveGameProgress("/Users/a0000/Documents/Games/savegames/game1.dat", game1);
-        saveGameProgress("/Users/a0000/Documents/Games/savegames/game2.dat", game2);
-        saveGameProgress("/Users/a0000/Documents/Games/savegames/game3.dat", game3);
+        saveGameProgress("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game1.dat", game1);
+        saveGameProgress("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game2.dat", game2);
+        saveGameProgress("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game3.dat", game3);
 
         ArrayList games = new ArrayList<>();
         games.add("game1.dat");
         games.add("game2.dat");
         games.add("game3.dat"); // исправил (было game2.dat)
 
-        zipFiles("/Users/a0000/Documents/Games/savegames/games.zip", games);
+        zipFiles("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/games.zip", games);
 
-        File dat1 = new File("/Users/a0000/Documents/Games/savegames/game1.dat");
-        File dat2 = new File("/Users/a0000/Documents/Games/savegames/game2.dat");
-        File dat3 = new File("/Users/a0000/Documents/Games/savegames/game3.dat");
+        File dat1 = new File("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game1.dat");
+        File dat2 = new File("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game2.dat");
+        File dat3 = new File("/Users/a0000/Documents/Java/JavaCore3_2/Games/savegames/game3.dat");
         dat1.delete();
         dat2.delete();
         dat3.delete();
